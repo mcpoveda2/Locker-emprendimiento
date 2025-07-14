44c510bc1d564568ececc9cc1be3f4a613ed319d
unknown <mcpoveda@espol.edu.ec>
2025-07-14 15:30:57 -0500

    Deploy inicial

--- src/pages/Generator.tsx
+++ src/pages/Generator.tsx
@@ -1,8 +1,9 @@
 
-
 function Generator(){
     return(
-        <h1>Generator</h1>
+        <h1 className="text-4xl font-bold text-blue-600">
+         ¡NativeWind está funcionando correctamente!
+      </h1>
     );
 }
 
